e7ff536993d12bc9607add2563eb884e9ac4722f
alamincoders <developeralamin03@gmail.com>
2022-09-19 21:55:21 +0600

    server

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
 const auth = require("json-server-auth");
-const jsonServer = require("json-server");
 const express = require("express");
 const http = require("http");
-
 const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-
+const data = require("./db.js");
+const jsonServer = require("json-server");
+const middleware = jsonServer.defaults();
+const router = jsonServer.router(data);
+const port = process.env.PORT || 9000;
 global.io = io;
 
-const router = jsonServer.router("db.json");
+/* server.use(middleware);
+server.use(router); */
+
 
 // response middleware
 router.render = (req, res) => {
     const path = req.path;
     const method = req.method;
-
-    if (
-        path.includes("/conversations") &&
-        (method === "POST" || method === "PATCH")
-    ) {
+    
+    if (path.includes("/conversations") && (method === "POST" || method === "PATCH")) {
         // emit socket event
         io.emit("conversation", {
             data: res.locals.data,
         });
     }
-
+    
     res.json(res.locals.data);
 };
-
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 9000;
 
 // Bind the router db to the app
 app.db = router.db;
 
-app.use(middlewares);
+app.use(middleware);
 
 const rules = auth.rewriter({
-    users: 640,
-    conversations: 660,
-    messages: 660,
+  users: 640,
+  conversations: 660,
+  messages: 660,
 });
 
 app.use(rules);
 app.use(auth);
 app.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log("listening on port " + port);
+});
